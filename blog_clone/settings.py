1a56206b89ca145b8204c41e8b1546d6fc8d35d5
conniekang <hqk1121@gmail.com>
2021-09-16 14:40:18 -0400

    built base html file

--- blog_clone/settings.py
+++ blog_clone/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / "static"
+
 
 LOGIIN_REDIRECT_URL = '/'
 
--- blog_clone/urls.py
+++ blog_clone/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogs:urls')),
-    path('accounts/login/', views.login, name='login'),
-    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page':'/'}),
-]
+    path('', include('blog.urls')),
+    path('accounts/login/', views.LoginView, name='login'),
+    path('accounts/logout/', views.LogoutView, name='logout', kwargs={'next_page':'/'}),
+] 
